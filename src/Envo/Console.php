8c61707a3a20eb08be3a23d2b617223bbcaeb903
Omer Mohamed <blinness@gmail.com>
2017-07-12 12:11:53 +0200

    Add symfony console component instead of phalcon

--- src/Envo/Console.php
+++ src/Envo/Console.php
@@ -11,6 +11,7 @@ use Phalcon\Exception as PhalconException;
 use Phalcon\Di\FactoryDefault\Cli as CliDI;
 use Phalcon\Cli\Console as ConsoleApp;
 use Phalcon\Db\Adapter\Pdo\Mysql as Database;
+use Symfony\Component\Console\Application;
 
 class Console
 {
@@ -25,57 +26,14 @@ class Console
 
     public function start()
     {
-        $this->setup();
-        $this->setupConfig();
+        $app = new Application('envome', '0.0.1');
 
-        $this->listen();
-    }
+        $app->add((new \Phinx\Console\Command\Migrate())->setName('phinx:migrate'));
+        $app->add((new \Phinx\Console\Command\Init())->setName('phinx:init'));
+        $app->add((new \Phinx\Console\Command\Rollback())->setName('phinx:rollback'));
+        $app->add((new \Phinx\Console\Command\Status())->setName('phinx:status'));
+        $app->add((new \Phinx\Console\Command\Create())->setName('phinx:create'));
 
-    public function listen()
-    {
-        $di = new CliDI();
-
-        /**
-         * Set the database configuration
-         */
-        $di->set('db', function () use ($di) {
-            $databaseConfig = require(APP_PATH . 'config/database.php');
-            $connection = new Database($databaseConfig['connections'][$databaseConfig['default']]);
-
-            return $connection;
-        });
-
-        // Create a console application
-        $console = new ConsoleApp();
-        $console->setDI($di);
-
-        /**
-         * Process the console arguments
-         */
-        $arguments = [
-            'task' => isset($this->argv[1]) ? ucfirst($this->argv[1]) : 'Main',
-            'action' => isset($this->argv[2]) ? $this->argv[1] : null,
-            'params' => isset($this->argv[3]) ? $this->argv[1] : null,
-        ];
-
-        $arguments['task'] = 'Envo\Command\\' . ucfirst($arguments['task']);
-
-        // Define global constants for the current task and action
-        define('CURRENT_TASK',   (isset($this->argv[1]) ? $this->argv[1] : null));
-        define('CURRENT_ACTION', (isset($this->argv[2]) ? $this->argv[2] : null));
-
-        $vendor = sprintf('Envo (%s)', env('APP_VERSION'));
-        print $vendor ."\n";
-        print "Date: " . Date::now() . PHP_EOL . PHP_EOL;
-
-        try {
-            // Handle incoming arguments
-            $console->handle($arguments);
-        } catch (\Exception $e) {
-            echo $e->getMessage();
-
-            die(var_dump($e));
-            exit(255);
-        }
+        $app->run();
     }
 }
