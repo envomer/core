4cbdf377c7383f9529f0e2b007e79c4f596bcd64
Omer Mohamed Ali <blinness@gmail.com>
2017-10-07 19:46:25 +0200

    Fix slack integration

--- src/Envo/Notification/Provider/Slack.php
+++ src/Envo/Notification/Provider/Slack.php
@@ -9,6 +9,8 @@ class Slack implements ProviderInterface
 {
     protected $webhookUrl;
 
+    public $icon = ':smirk:';
+
     /**
      * Pushover construct
      */
@@ -19,40 +21,34 @@ class Slack implements ProviderInterface
 
     /**
      * Send notification
+     * 
+     * Guide: https://api.slack.com/incoming-webhooks
      *
      * @param Notification $notification
      * @return void
      */
     public function send(Notification $notification)
     {
-        $channel = '#server';
-        $message = '@om. this tool is working good';
-
-        $ch = curl_init("https://slack.com/api/chat.postMessage");
-        $data = http_build_query([
-            // "token" => 'xoxp-252980242100-252924753042-254027159015-1bf936028690a27cd5a18275577417dd',
-            // "token" => 'xoxb-254030406039-nNtjy5cAlmmhVjKjEhFDTfku',
-            "token" => 'xoxb-253000670772-lrn8gD9lzEWJ18Ze6XgGZaWf',
-            "channel" => $channel, //"#mychannel",
-            "text" => $message, //"Hello, Foo-Bar channel message.",
-            "username" => "envobot",
-            // 'as_user' => true
+        $ch = curl_init($this->webhookUrl);
+        $data = json_encode([
+            'text' => $notification->getBody(),
+            'username' => 'serverbot',
+            'icon_emoji' => $this->icon,
+            // 'channel' => $notification->
         ]);
+        
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
         curl_close($ch);
-	
 
         die(var_dump($response));
-
-        // TODO: return response..
     }
 
     /**
-     * Validate pushover service
+     * Validate slack service
      *
      * @return bool
      */
