e7f79e024fbcc53fa82c359b66ed164d4681ec87
Omer Mohamed Ali <om@anexia.at>
2017-11-04 19:36:16 +0100

    Change: update maintenance design

--- src/Envo/View/html/maintenance.php
+++ src/Envo/View/html/maintenance.php
@@ -5,14 +5,19 @@
 
 	<style type="text/css">
 		*{ box-sizing: border-box }
-		html{font-size:100%;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}
-		html,button,input,select,textarea{font-family:sans-serif}
-		body, html { margin: 0; padding: 0; background: #fff; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; color: #23262B; text-align: center; height: 100%; }
-		body { font-weight:100; font-size:13px; }
-		body { background: radial-gradient(ellipse at center, #222 0%,#111 68%,#0f0f15 100%); color: #ddd }
+		html{
+            -webkit-text-size-adjust:100%;
+            font-family: -apple-system, BlinkMacSystemFont, "myriad-pro", sans-serif;
+            font-weight: 400;
+            background-color: #ffffff;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+		body, html { margin: 0; padding: 0; background: #fff; color: #23262B; text-align: center; height: 100%; }
+		body {  font-size:13px; color: #ddd }
 		h1 { color: #19B5FE; padding: 0 50px; width: 400px; margin: 50pt auto 50px; text-align: center; font-size: 60pt; }
 		h1 b, h3 span { color: #E04157; }
-        h3 { max-width: 600px; width: 90%; margin: auto; margin-top: 20%; z-index: 1; position: relative; }
+        h3 { max-width: 700px; width: 90%; margin: 20% auto auto; z-index: 1; position: relative; padding: 60px 40px; font-weight: normal }
 		p { color: rgba(0,0,0,0.8); font-size: 14pt; margin: 0; }
 		p span { color: #2ABB9B; }
 
@@ -21,17 +26,28 @@
 
 		.right { text-align: right; }
 
-		.footer { position: absolute; bottom: 40px; border-top: 1px solid rgba(255,255,255,0.1); width: 90%; max-width: 600px; margin: auto; left: 0; right: 0; padding-top: 10px; font-size: 13pt }
-		.footer div { display: inline-block; width: 32%; vertical-align: top; color: #ccc; font-weight: bold }
-		.footer b { color: #19B5FE }
+		.footer { position: absolute; bottom: 20px; margin: auto; left: 0; right: 0;; font-size: 11pt; background: #000; padding: 15px; }
+		.footer div { display: inline-block; width: 32%; vertical-align: top; color: #ccc; }
+		.footer div:first-child b { color: #19B5FE }
+		.footer div:nth-child(2) b { color: #7b22aa }
+		.footer div:last-child b { color: #b223a1 }
 		.footer div:first-child { text-align: left; }
 		.footer div:last-child { text-align: right; }
+        .sub { position: absolute; bottom: 70px; height: 5px; left: 0; right: 0; }
 
 		#copy { position: fixed; top: 50px; right: 60px; height: 50px; width: 100px }
         #copy img { max-height: 100px; max-width: 100px; opacity: 0.8; }
+        .purple {
+            background: #b223a1;
+            background: -moz-linear-gradient(-45deg, #b223a1 0%, #7b22aa 27%, #271f9b 55%, #322cd6 96%);
+            background: -webkit-linear-gradient(-45deg, #b223a1 0%,#7b22aa 27%,#271f9b 55%,#322cd6 96%);
+            background: linear-gradient(135deg, #b223a1 0%,#7b22aa 27%,#271f9b 55%,#322cd6 96%);
+            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#b223a1', endColorstr='#322cd6',GradientType=1 );
+        }
+        .purple2 { background: #22074D; }
 	</style>
 </head>
-<body>
+<body class="purple2">
     <div class="not-found">
         <i class="ex ex-sad ex-7x"></i>
         <h3>
@@ -47,7 +63,8 @@
 	<div class="footer">
 		<div class="part">Duration: <b> ~<?php echo number_format($maintenance->retry/60, 2) ?> Hour</b></div>
 		<div class="part">Progress: <b><?php echo $maintenance->progress  ?>%</b></div>
-		<div class="part">- <b><?php echo env('APP_NAME', 'envo') ?></b></div>
+		<div class="part"><b><?php echo env('APP_NAME', 'envo') ?></b></div>
 	</div>
+    <div class="sub purple"></div>
 </body>
 </html>
