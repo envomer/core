1eb1c0f6d7803e86a577c58ebf3e1359cd1ada50
Omer Mohamed Ali <om@anexia.at>
2018-01-22 20:26:13 +0100

    [core] Add footer email template

--- src/Envo/Mail/Transport/TransportInterface.php
+++ src/Envo/Mail/Transport/TransportInterface.php
@@ -2,7 +2,12 @@
 
 namespace Envo\Mail\Transport;
 
+use Envo\Extension\EmailTemplate\ResponseDTO;
+
 interface TransportInterface
 {
+	/**
+	 * @return ResponseDTO
+	 */
 	public function send();
 }
