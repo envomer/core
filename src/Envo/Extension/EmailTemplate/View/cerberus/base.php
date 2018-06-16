b98db300f54a56acca44bb171d157e43edab568f
Omer Mohamed Ali <om@anexia.at>
2018-06-16 13:13:34 +0200

    Change: reduce font size and lighten background color

--- src/Envo/Extension/EmailTemplate/View/cerberus/base.php
+++ src/Envo/Extension/EmailTemplate/View/cerberus/base.php
@@ -2,9 +2,9 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
 	<meta charset="utf-8"> <!-- utf-8 works for most cases -->
-	<meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
-	<meta name="x-apple-disable-message-reformatting">  <!-- Disable auto-scale in iOS 10 Mail entirely -->
+	<meta name="viewport" content="width=device-width"> <?php // Forcing initial-scale shouldn't be necessary ?>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> <?php // Use the latest (edge) version of IE rendering engine ?>
+	<meta name="x-apple-disable-message-reformatting">  <?php // Disable auto-scale in iOS 10 Mail entirely ?>
 	<title></title>
 
 	<?php 
@@ -25,6 +25,7 @@
 		</style>
 	<![endif]-->
 	<?php // All other clients get the webfont reference; some will render the font and others will silently fail to the fallbacks. More on that here: http://stylecampaign.com/blog/2015/02/webfont-support-in-email/ ?>
+
 	<!--[if !mso]><!-->
 	<!-- insert web font reference, eg: <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'> -->
 	<!--<![endif]-->
@@ -36,52 +37,28 @@
 		
 		<?php //What it does: Remove spaces around the email design added by some email clients. ?>
 		<?php // Beware: It can remove the padding / margin and add a background color to the compose a reply window. ?>
-		html,
-		body {
-			margin: 0 auto !important;
-			padding: 0 !important;
-			height: 100% !important;
-			width: 100% !important;
-		}
-		
+		html, body {margin: 0 auto !important; padding: 0 !important; height: 100% !important; width: 100% !important;}
+		body, p, div { font-family: arial; font-size: 14px; }
+		body { color: #000000; }
+
 		<?php //What it does: Stops email clients resizing small text. ?>
-		* {
-			-ms-text-size-adjust: 100%;
-			-webkit-text-size-adjust: 100%;
-		}
+		* {-ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; }
 		
 		<?php //What it does: Centers email on Android 4.4 ?>
-		div[style*="margin: 16px 0"] {
-			margin: 0 !important;
-		}
+		div[style*="margin: 16px 0"] { margin: 0 !important; }
 		
 		<?php //What it does: Stops Outlook from adding extra spacing to tables. ?>
-		table,
-		td {
-			mso-table-lspace: 0pt !important;
-			mso-table-rspace: 0pt !important;
-		}
+		table, td { mso-table-lspace: 0pt !important; mso-table-rspace: 0pt !important; }
 		
 		<?php //What it does: Fixes webkit padding issue. Fix for Yahoo mail table alignment bug. Applies table-layout to the first 2 tables then removes for anything nested deeper. ?>
-		table {
-			border-spacing: 0 !important;
-			border-collapse: collapse !important;
-			table-layout: fixed !important;
-			margin: 0 auto !important;
-		}
-		table table table {
-			table-layout: auto;
-		}
+		table { border-spacing: 0 !important; border-collapse: collapse !important; table-layout: fixed !important; margin: 0 auto !important; }
+		table table table { table-layout: auto; }
 		
 		<?php //What it does: Uses a better rendering method when resizing images in IE. ?>
-		img {
-			-ms-interpolation-mode:bicubic;
-		}
+		img { -ms-interpolation-mode:bicubic; }
 		
 		<?php //What it does: A work-around for email clients meddling in triggered links. ?>
-		*[x-apple-data-detectors],  /* iOS */
-		.x-gmail-data-detectors,    /* Gmail */
-		.x-gmail-data-detectors *,
+		*[x-apple-data-detectors], .x-gmail-data-detectors, .x-gmail-data-detectors *,
 		.aBn {
 			border-bottom: 0 !important;
 			cursor: default !important;
@@ -94,19 +71,13 @@
 		}
 		
 		<?php //What it does: Prevents Gmail from displaying an download button on large, non-linked images. ?>
-		.a6S {
-			display: none !important;
-			opacity: 0.01 !important;
-		}
+		.a6S { display: none !important; opacity: 0.01 !important; }
+
 		<?php //If the above doesn't work, add a .g-img class to any image in question. ?>
-		img.g-img + div {
-			display: none !important;
-		}
+		img.g-img + div { display: none !important; }
 		
 		<?php //What it does: Prevents underlining the button text in Windows 10 ?>
-		.button-link {
-			text-decoration: none !important;
-		}
+		.button-link { text-decoration: none !important; }
 		
 		<?php
 			//What it does: Removes right gutter in Gmail iOS app: https://github.com/TedGoas/Cerberus/issues/89
@@ -114,15 +85,11 @@
 			// Thanks to Eric Lepetit @ericlepetitsf) for help troubleshooting
 		?>
 		@media only screen and (min-device-width: 375px) and (max-device-width: 413px) { /* iPhone 6 and 6+ */
-			.email-container {
-				min-width: 375px !important;
-			}
+			.email-container { min-width: 375px !important; }
 		}
 		
 		<?php // What it does: Forces Gmail app to display email full width ?>
-		u ~ div .email-container {
-			min-width: 100vw;
-		}
+		u ~ div .email-container { min-width: 100vw; }
 	
 	</style>
 	<!-- CSS Reset : END -->
@@ -131,63 +98,32 @@
 	<style>
 		
 		<?php // What it does: Hover styles for buttons ?>
-		.button-td,
-		.button-a {
-			transition: all 100ms ease-in;
-		}
-		.button-td:hover,
-		.button-a:hover {
-			background: #555555 !important;
-			border-color: #555555 !important;
-		}
+		.button-td, .button-a { transition: all 100ms ease-in; }
+		.button-td:hover, .button-a:hover { background: #555555 !important; border-color: #555555 !important; }
 		
 		/* Media Queries */
 		@media screen and (max-width: 480px) {
-			
 			<?php // What it does: Forces elements to resize to the full width of their container. Useful for resizing images beyond their max-width. ?>
-			.fluid {
-				width: 100% !important;
-				max-width: 100% !important;
-				height: auto !important;
-				margin-left: auto !important;
-				margin-right: auto !important;
-			}
+			.fluid { width: 100% !important; max-width: 100% !important; height: auto !important; margin-left: auto !important; margin-right: auto !important; }
 			
 			<?php // What it does: Forces table cells into full-width rows. ?>
-			.stack-column,
-			.stack-column-center {
-				display: block !important;
-				width: 100% !important;
-				max-width: 100% !important;
-				direction: ltr !important;
-			}
+			.stack-column, .stack-column-center { display: block !important; width: 100% !important; max-width: 100% !important; direction: ltr !important; }
+
 			/* And center justify these ones. */
-			.stack-column-center {
-				text-align: center !important;
-			}
+			.stack-column-center { text-align: center !important; }
 			
 			<?php // What it does: Generic utility class for centering. Useful for images, buttons, and nested tables. ?>
-			.center-on-narrow {
-				text-align: center !important;
-				display: block !important;
-				margin-left: auto !important;
-				margin-right: auto !important;
-				float: none !important;
-			}
-			table.center-on-narrow {
-				display: inline-block !important;
-			}
+			.center-on-narrow { text-align: center !important; display: block !important; margin-left: auto !important; margin-right: auto !important; float: none !important; }
+			table.center-on-narrow { display: inline-block !important; }
 			
 			<?php // What it does: Adjust typography on small screens to improve readability ?>
-			.email-container p {
-				font-size: 17px !important;
-			}
+			.email-container p { font-size: 17px !important; }
 		}
 	
 	</style>
 	<!-- Progressive Enhancements : END -->
 	
-	<!-- What it does: Makes background images in 72ppi Outlook render at correct size. -->
+	<?php // What it does: Makes background images in 72ppi Outlook render at correct size. ?>
 	<!--[if gte mso 9]>
 	<xml>
 		<o:OfficeDocumentSettings>
@@ -198,8 +134,8 @@
 	<![endif]-->
 
 </head>
-<body width="100%" style="margin: 0; mso-line-height-rule: exactly;" bgcolor="<?php echo $this->getStyle('backgroundColor', '#d7d7d7') ?>">
-    <center style="width: 100%; background: <?php echo $this->getStyle('backgroundColor', '#d7d7d7') ?>; text-align: left;">
+<body width="100%" style="margin: 0; mso-line-height-rule: exactly;" bgcolor="<?php echo $this->getStyle('backgroundColor', '#ebebeb') ?>">
+    <center style="width: 100%; background: <?php echo $this->getStyle('backgroundColor', '#ebebeb') ?>; text-align: left;">
     <!--[if mso | IE]>
     <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#222222">
     <tr>
@@ -279,7 +215,7 @@
     
 
     <?php if($this->footer || $this->pixelPath): ?>
-    <div style="width: 100%; background: <?php echo '#ccc' ?: '#222222' ?>; text-align: left; text-align: center;">
+    <div style="width: 100%; background: <?php echo '#e4e4e4' ?: '#222222' ?>; text-align: left; text-align: center;">
     	<?php
         /**
 			Set the email width. Defined in two places:
