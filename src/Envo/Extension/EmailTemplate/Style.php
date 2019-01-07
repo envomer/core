169537413a27bfef91a2053aaa947c70b407332e
Omer Mohamed Ali <om@anexia.at>
2019-01-07 11:41:33 +0100

    Add 'adjustColorBrightness' to email templates

--- src/Envo/Extension/EmailTemplate/Style.php
+++ src/Envo/Extension/EmailTemplate/Style.php
@@ -6,9 +6,28 @@ use Envo\AbstractDTO;
 
 class Style extends AbstractDTO
 {
+	/**
+	 * @var string
+	 */
 	public $backgroundColor;
+	
+	/**
+	 * @var string
+	 */
 	public $containerColor;
+	
+	/**
+	 * @var string
+	 */
 	public $color;
+	
+	/**
+	 * @var string
+	 */
 	public $btnColor;
+	
+	/**
+	 * @var string
+	 */
 	public $btnBackgroundColor;
 }
